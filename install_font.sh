8e2f7a3128bef4a273bf969139c79674bff55f79
Matthias Guenther <matthias@wikimatze.de>
2015-01-21 07:06:33 +0100

    Put Droid Sans font into ~/.fonts folder

--- install_font.sh
+++ install_font.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-curl -fLo /tmp/DroidSansMonoForPowerlinePlusNerdFileTypes.otf \
-    https://raw.githubusercontent.com/ryanoasis/vim-webdevicons/master/lib/DroidSansMonoForPowerlinePlusNerdFileTypes.otf
-cd /tmp && sudo chmod 777 DroidSansMonoForPowerlinePlusNerdFileTypes.otf
-
-if [ "$(uname)" == "Linux" ]
-then
-  sudo cp /tmp/DroidSansMonoForPowerlinePlusNerdFileTypes.otf /usr/share/fonts/opentype
-fi
+cd ~/.fonts && curl -fLo DroidSansMonoForPowerlinePlusNerdFileTypes.otf https://raw.githubusercontent.com/ryanoasis/nerd-filetype-glyphs-fonts-patcher/master/patched-fonts/Droid%20Sans%20Mono%20for%20Powerline%20Plus%20Nerd%20File%20Types.otf && sudo chmod 777 DroidSansMonoForPowerlinePlusNerdFileTypes.otf
 
