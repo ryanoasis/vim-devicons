770ea0df21dd762e36e35da8691b49cf469d5c9e
delphinus <delphinus@remora.cx>
2015-07-05 12:03:08 +0900

    use hardtab

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+# vim:se fenc=utf8 noet:
 from setuptools import setup, find_packages
 
 setup(
-    name='powerlinex-segment-webdevicons',
-    packages=find_packages(),
-    namespace_packages=['powerlinex'],
-    )
+		name='powerlinex-segment-webdevicons',
+		packages=find_packages(),
+		namespace_packages=['powerlinex'],
+		)
