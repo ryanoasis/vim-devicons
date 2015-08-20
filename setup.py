5c311d01748f1f1d8eb282ca6f98abb3b7c2b0d1
delphinus <delphinus@remora.cx>
2015-08-20 22:33:01 +0900

    remove unnecessary setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# vim:se fenc=utf8 noet:
-from setuptools import setup, find_packages
-
-setup(
-		name='powerlinex-segment-webdevicons',
-		packages=find_packages(),
-		namespace_packages=['powerlinex'],
-		)
